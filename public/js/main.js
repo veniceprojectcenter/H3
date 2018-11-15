3d973a53249802725b455f0bbbc38e254eb179eb
Peter Maida <pmaida1357@gmail.com>
2018-11-15 17:40:38 +0100

    Added scrollmagic

--- public/js/main.js
+++ public/js/main.js
@@ -14,9 +14,22 @@ for (let companyCard of companyCards) {
     )
 }*/
 
+// Companies
 // https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_slide_toggle
 $(document).ready(function() {
     $("#flip").hover(function() {
         $("#panel").slideToggle(300);
     });
 });
+
+
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: '#pinned-trigger1', // starting scene, when reaching this element
+  duration: 400 // pin the element for a total of 400px
+})
+.setPin('#pinned-element1'); // the element we want to pin
+
+// Add Scene to ScrollMagic Controller
+controller.addScene(scene);
--- themes/H3-Theme/static/js/main.js
+++ themes/H3-Theme/static/js/main.js
@@ -14,9 +14,35 @@ for (let companyCard of companyCards) {
     )
 }*/
 
+// Companies
 // https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_slide_toggle
 $(document).ready(function() {
     $("#flip").hover(function() {
         $("#panel").slideToggle(300);
     });
+});
+
+
+/*var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: '#carousel', // starting scene, when reaching this element
+  duration: 400 // pin the element for a total of 400px
+})
+.setPin('#carousel'); // the element we want to pin
+
+// Add Scene to ScrollMagic Controller
+controller.addScene(scene);*/
+
+$(function() {
+    var controller = new ScrollMagic.Controller();
+
+    var containerScene = new ScrollMagic.Scene({
+        triggerElement: '#container',
+        duration: 500
+    })
+    .setPin('#block')
+    .addIndicators()
+    .addTo(controller);
+
 });
